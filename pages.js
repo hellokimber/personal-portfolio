8b2f11fbb380cee24bedf933e07c3dd7f5c64669
Kim vanderHelm <kvanderhelm@me.com>
2022-06-26 10:15:49 -0600

    fixed error in JS to add page

--- pages.js
+++ pages.js
@@ -7,7 +7,7 @@ const pages = [
   {copy: 'updating her portfolio', background: '#A9FFF7', circle: '#e34a47'},
   {copy: 'creating experiences for print and web', background: '#BBBE64', circle: '#A9FFF7'},
   {copy: 'posting photos on <a href="https://vsco.com/hellokimber">VSCO</a>', background: '#FDE74C', circle: '#b472e6'},
-  {copy: `sharing her resume on <a href="https://www.linkedin.com/in/kvanderhelm">LinkedIn</a>`, background: '#f18d8c', circle: '#A9FFF7'}
+  {copy: `sharing her resume on <a href="https://www.linkedin.com/in/kvanderhelm">LinkedIn</a>`, background: '#f18d8c', circle: '#A9FFF7'},
   {copy: `diving into code on <a href="https://github.com/hellokimber">GitHub</a>`, background: 'A9FFF7', circle: '#A9FFF7'}
 ]
 
